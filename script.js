6ba632392e9af420edc2a78f6350566518c29284
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-08 16:07:06 +0200

    created const

---
+++ script.js
@@ -0,0 +1,7 @@
+const userDistance=prompt(write how many km you want to travel);
+const userAge=prompt(type your Age);
+
+
+console.log(userDistance + userAge);
+document.getElementById('output').innerHTML= userPrice;
+
