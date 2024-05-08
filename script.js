2b1297d134f088008ecd71008fdb088add6f6853
MarcoSac1 <marcosaccone8@gmail.com>
2024-05-08 17:04:34 +0200

    agiunta variabili e condizioni

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const userDistance=prompt(write how many km you want to travel);
-const userAge=prompt(type your Age);
+// 1. Chiedo all'utente i kilometri
+// 2. Chiedo all'utente l'età
+// 3. Dichiaro un prezzo al kilometro
+// 4. Moltiplico il prezzo al chilometro per i kilometri e ottengo il prezzo della corsa
+// 5. SE l'utente è minorenne
+//     1. Sottraggo il 21% dal prezzo intero
+// 6. ALTRIMENTI SE l'utente è over 65
+//     1. Sottraggo il 41% dal prezzo intero
+// 7. Stampo il prezzo finale
 
 
-console.log(userDistance + userAge);
-document.getElementById('output').innerHTML= userPrice;
+const userDistance=prompt('write how many km you want to travel');
+const userAge=prompt('type your Age');
+const priceCost= 0.276;
 
+let tripCost = userDistance * priceCost;
+
+if(userAge < 18){
+    (tripCost / 100) * 21
+} else() {
+
+}
+
+
+console.log(tripCost);
+// document.getElementById('output').innerHTML= userPrice;
